1e0ea1ec6599275cfabe6933747c11baf666d682
jlmortola <jlmortola@me.com>
2020-03-04 23:27:21 +0100

    feat: add email validation

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,13 @@ import bcrypt from 'bcryptjs'
 const userSchema = new mongoose.Schema({
   name: String,
   lastName: String,
-  email: String,
+  email: {
+    type: String,
+    validate: {
+      validator: (email) => User.isUnique({email}),
+      message: props => `${props.value} is already registered`
+    },
+  },
   password: String,
 }, {
   timestamps: true
@@ -12,12 +18,13 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', function() {
-console.log("function")
-  
   const hashedPassword = bcrypt.hashSync(this.password, 12);
-  console.log("hashedPassword", hashedPassword)
   this.password = hashedPassword;
 });
+
+userSchema.statics.isUnique = async function(option) {
+  return await this.where(option).countDocuments() === 0
+}
 
 const User = mongoose.model('User', userSchema);
 
